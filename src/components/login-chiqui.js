8baf21ba91556026fa16adfd3fad070b7f528686
juniorots <juniorots@gmail.com>
2022-02-28 17:57:23 -0300

    DENTISTAS [ ORTHO CHIQUI ] - FRONT END

--- src/components/login-chiqui.js
+++ src/components/login-chiqui.js
@@ -20,49 +20,49 @@ export default class LoginChiqui extends Component {
 
     render() {
         return (
-            <div class="container">
+            <div className="container">
                 <HeaderChiqui />
-                <div class="row justify-content-center">
-                    <div class="col-xl-10 col-lg-12 col-md-9">
-                        <div class="card o-hidden border-0 shadow-lg my-5">
-                            <div class="card-body p-0">
-                                <div class="row">
-                                    <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                                        <div class="col-lg-10">
-                                            <div class="p-5">
-                                                <div class="text-center">
+                <div className="row justify-content-center">
+                    <div className="col-xl-10 col-lg-12 col-md-9">
+                        <div className="card o-hidden border-0 shadow-lg my-5">
+                            <div className="card-body p-0">
+                                <div className="row">
+                                    <div className="col-lg-6 d-none d-lg-block bg-login-image">
+                                        <div className="col-lg-10">
+                                            <div className="p-5">
+                                                <div className="text-center">
                                                     <span style={{...styleBase, ...styleAzul}} >
                                                         DENTISTAS [ 
-                                                            <span style={{...styleBase, ...styleVermelho }}> ORTHO CHIQUI </span> 
+                                                            <span style={{...styleBase, ...styleVermelho}}> ORTHO CHIQUI </span> 
                                                         ]</span>                                                                                                                                                                
                                                 </div>
-                                                <div class="user">
-                                                    <div class="form-group">
+                                                <form className="user">
+                                                    <div className="form-group">
                                                         <input 
                                                             type="login" 
-                                                            class="form-control form-control-user"
+                                                            className="form-control form-control-user"
                                                             value={this.login}                                                            
                                                             placeholder="LOGIN" 
                                                             onChange={value => this.onChangeHandler(value)}
                                                             />
                                                     </div>
-                                                    <div class="form-group">
+                                                    <div className="form-group">
                                                         <input 
-                                                            type="password" 
-                                                            class="form-control form-control-user"
+                                                            type="new-password" 
+                                                            className="form-control form-control-user"
                                                             name="pwd" 
                                                             value={this.pwd}
                                                             onChange={value => this.onChangeHandler(value)}
                                                             placeholder="***"/>
                                                     </div>
-                                                    <div class="from-group">
+                                                    <div className="from-group">
                                                         <button                                     
                                                             className="btn btn-primary"
                                                             onClick={() => this.tryLogin()}>
                                                                 Entrar
                                                         </button>
                                                     </div>
-                                                </div>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
